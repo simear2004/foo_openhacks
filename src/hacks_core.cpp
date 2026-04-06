1f6899adade84f7c38b87339c296e6ca1ce1d24c
YeYo <16457576+simear2004@users.noreply.github.com>
2026-04-06 14:58:35 +0800

    修改响应边界为 2

--- src/hacks_core.cpp
+++ src/hacks_core.cpp
@@ -89,8 +89,8 @@ bool OpenHacksCore::IsMainOrChildWindow(HWND wnd)
 
 POINT OpenHacksCore::GetBorderMetrics()
 {
-    const int32_t cx = Utility::GetSystemMetricsForDpi(SM_CXFRAME, OpenHacksVars::DPI) + Utility::GetSystemMetricsForDpi(SM_CXPADDEDBORDER, OpenHacksVars::DPI);
-    const int32_t cy = Utility::GetSystemMetricsForDpi(SM_CYFRAME, OpenHacksVars::DPI) + Utility::GetSystemMetricsForDpi(SM_CXPADDEDBORDER, OpenHacksVars::DPI);
+    const int32_t cx = 2;
+    const int32_t cy = 2;
     return POINT{cx, cy};
 }
 
