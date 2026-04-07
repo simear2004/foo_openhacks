098595c40cc6fb99eab0f3db6877c27fb2b29302
YeYo <16457576+simear2004@users.noreply.github.com>
2026-04-07 14:34:16 +0800

    Update hacks_core.h

--- src/hacks_core.h
+++ src/hacks_core.h
@@ -1,118 +1,55 @@
 #pragma once
+#include <type_traits>
 #include <optional>
-#include "win32_utils.h"
+#include <Windows.h>
 
-enum HacksInitErrors : uint32_t
+enum WindowFrameStyle
 {
-    NoError = 0,
-    HooksInstallError = 1 << 0,
-    IncompatibleComponentInstalled = 1 << 1,
+    WindowFrameStyleDefault = 0,
+    WindowFrameStyleNoCaption,
+    WindowFrameStyleNoBorder,
 };
 
-class OpenHacksCore
+struct WindowState
 {
-public:
-    static OpenHacksCore& Get();
-
-    FORCEINLINE bool HasInitError() const
-    {
-        return mInitErrors != HacksInitErrors::NoError;
-    }
-
-    void Initialize();
-    void Finalize();
-
-    bool InstallWindowHooks();
-
-    void ToggleStatusBar();
-    void ToggleMenuBar();
-
-    void ShowOrHideStatusBar(bool value);
-    bool ShowOrHideMenuBar(bool value);
-
-    bool CheckIncompatibleComponents();
-
-    void ApplyMainWindowFrameStyle(WindowFrameStyle newStyle);
-
-    void Maximize();
-    void Restore();
-    bool IsMaximized();
-    bool IsMinimized();
-
-    // Fullscreen operations
-    void EnterFullscreen();
-    void ExitFullscreen();
-    void ToggleFullscreen();
-
-private:
-    FORCEINLINE static LRESULT CALLBACK StaticOpenHacksMainWindowProc(HWND wnd, UINT msg, WPARAM wp, LPARAM lp)
-    {
-        return Get().OpenHacksMainWindowProc(wnd, msg, wp, lp);
-    }
-
-    FORCEINLINE static LRESULT CALLBACK StaticOpenHacksCallWndProc(int code, WPARAM wp, LPARAM lp)
-    {
-        return Get().OpenHacksCallWndProc(code, wp, lp);
-    }
-
-    FORCEINLINE static LRESULT CALLBACK StaticOpenHacksGetMessageProc(int code, WPARAM wp, LPARAM lp)
-    {
-        return Get().OpenHacksGetMessageProc(code, wp, lp);
-    }
-
-    FORCEINLINE static LRESULT CALLBACK StaticOpenHacksStatusBarProc(HWND wnd, UINT msg, WPARAM wp, LPARAM lp)
-    {
-        return Get().OpenHacksStatusBarProc(wnd, msg, wp, lp);
-    }
-
-    FORCEINLINE static LRESULT CALLBACK StaticOpenHacksReBarProc(HWND wnd, UINT msg, WPARAM wp, LPARAM lp)
-    {
-        return Get().OpenHacksReBarProc(wnd, msg, wp, lp);
-    }
-
-    FORCEINLINE bool IsMenuBarVisible() const
-    {
-        return mMainMenuWindow != nullptr && IsWindowVisible(mMainMenuWindow);
-    }
-
-    LRESULT OpenHacksMainWindowProc(HWND wnd, UINT msg, WPARAM wp, LPARAM lp);
-    LRESULT OpenHacksCallWndProc(int code, WPARAM wp, LPARAM lp);
-    LRESULT OpenHacksGetMessageProc(int code, WPARAM wp, LPARAM lp);
-    LRESULT OpenHacksStatusBarProc(HWND wnd, UINT msg, WPARAM wp, LPARAM lp);
-    LRESULT OpenHacksReBarProc(HWND wnd, UINT msg, WPARAM wp, LPARAM lp);
-
-    void UninstallWindowHooks();
-    bool InstallWindowHooksInternal();
-
-    bool IsMainOrChildWindow(HWND wnd);
-    POINT GetBorderMetrics();
-    Rect GetRectForNonSizing();
-
-private:
-    // main window message handlers
-    bool OnSysCommand(HWND wnd, WPARAM wp, LPARAM lp);
-    LRESULT OnNCHitTest(HWND wnd, WPARAM wp, LPARAM lp);
-    bool OnSetCursor(HWND wnd, WPARAM wp, LPARAM lp);
-    bool OnSize(HWND wnd, WPARAM wp, LPARAM lp);
-    // windows hook handlers
-    void OnHookMouseMove(LPMSG msg);
-    void OnHookLButtonDown(LPMSG msg);
-    bool OnMove(HWND wnd, WPARAM wp, LPARAM lp);
-
-private:
-    HWND mMainWindow = nullptr;
-    HWND mRebarWindow = nullptr;
-    HWND mMainMenuWindow = nullptr;
-    HWND mStatusBar = nullptr;
-    HHOOK mCallWndHook = nullptr;
-    HHOOK mGetMsgHook = nullptr;
-    WNDPROC mMainWindowOriginProc = nullptr;
-    WNDPROC mStatusBarOriginProc = nullptr;
-    WNDPROC mReBarOriginProc = nullptr;
-
-    uint32_t mInitErrors = HacksInitErrors::NoError;
-    DWORD mInstallHooksWin32Error = ERROR_SUCCESS;
-
-    std::optional<WindowState> mSavedWindowState;
-    bool mRequireRevertCursor = false;
+    bool fullscreen = false;
+    DWORD style = 0;
+    WINDOWPLACEMENT wp = { sizeof(WINDOWPLACEMENT) };
 };
+
+namespace Utility
+{
+template <typename F, typename = std::enable_if_t<std::is_function<typename std::remove_pointer<F>::type>::value>>
+bool GetProcAddress(HMODULE h, const char* funcName, F& f)
+{
+    if (auto ptr = ::GetProcAddress(h, funcName))
+    {
+        f = reinterpret_cast<F>(ptr);
+        return true;
+    }
+
+    f = nullptr;
+    return false;
+}
+
+RECT& ClientToScreen(HWND wnd, RECT& rc);
+RECT& ScreenToClient(HWND wnd, RECT& rc);
+
+bool IsCompositionEnabled();
+bool IsWindows11OrGreater();
+bool EnableWindowShadow(HWND window, bool enable);
+uint32_t GetDPI(HWND window);
+int32_t GetSystemMetricsForDpi(int32_t index, uint32_t dpi);
+
+// Window state management
+void Maximize(HWND wnd, WindowState& state);
+void Restore(HWND wnd, WindowState& state);
+bool IsMaximized(HWND wnd);
+bool IsMinimized(HWND wnd);
+void ApplyWindowFrameStyle(HWND wnd, WindowFrameStyle style);
+
+// Fullscreen management
+void EnterFullscreen(HWND wnd, WindowState& state);
+void ExitFullscreen(HWND wnd, WindowState& state);
+bool IsFullscreen(HWND wnd);
+} // namespace Utility
