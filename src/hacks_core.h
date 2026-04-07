817a4d251c27fb0d846d209b331e06d35012f85a
YeYo <16457576+simear2004@users.noreply.github.com>
2026-04-07 14:08:54 +0800

    Update hacks_core.h

--- src/hacks_core.h
+++ src/hacks_core.h
@@ -97,7 +97,6 @@ private:
     // windows hook handlers
     void OnHookMouseMove(LPMSG msg);
     void OnHookLButtonDown(LPMSG msg);
-    bool OnMove(HWND wnd, WPARAM wp, LPARAM lp);
 
 private:
     HWND mMainWindow = nullptr;
