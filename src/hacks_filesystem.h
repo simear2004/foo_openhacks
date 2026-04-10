6366dcb0693519c13eaeb2883d821c1c4ce9a8d4
YeYo <16457576+simear2004@users.noreply.github.com>
2026-04-10 13:17:06 +0800

    Update hacks_filesystem.h

--- src/hacks_filesystem.h
+++ src/hacks_filesystem.h
@@ -11,15 +11,24 @@ public:
     bool is_our_path(const char* p_path) override;
     bool get_canonical_path(const char* p_path, pfc::string_base& p_out) override;
     bool get_display_path(const char* p_path, pfc::string_base& p_out) override;
+
     void open(service_ptr_t<file>& p_out, const char* p_path, t_open_mode p_mode, abort_callback& p_abort) override;
     void remove(const char* p_path, abort_callback& p_abort) override;
     void move(const char* p_src, const char* p_dst, abort_callback& p_abort) override;
+    
     bool is_remote(const char* p_src) override;
     void get_stats(const char* p_path, t_filestats& p_stats, bool& p_is_writeable, abort_callback& p_abort) override;
+    
     void create_directory(const char* p_path, abort_callback& p_abort) override;
     void list_directory(const char* p_path, directory_callback& p_out, abort_callback& p_abort) override;
+    
     bool supports_content_types() override;
     char pathSeparator() override;
+    
+    bool get_volume_name(const char* p_path, pfc::string_base& p_out) override { return false; }
+    bool are_paths_equal(const char* p_path1, const char* p_path2) override { return false; }
+    void copy(const char* p_src, const char* p_dst, abort_callback& p_abort) override { throw exception_io_unsupported(); }
+    void rename(const char* p_old_path, const char* p_new_path, abort_callback& p_abort) override { move(p_old_path, p_new_path, p_abort); }
 
 private:
     std::string ExpandFoobar2000Path(const std::string& fb2kPath) const;
