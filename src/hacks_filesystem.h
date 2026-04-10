a4f29f64e6cb4be0f43d257a8db6087d81720e57
YeYo <16457576+simear2004@users.noreply.github.com>
2026-04-10 13:36:55 +0800

    Update hacks_filesystem.h

--- src/hacks_filesystem.h
+++ src/hacks_filesystem.h
@@ -8,27 +8,21 @@ class filesystem_foobar2000 : public filesystem_v3
 public:
     filesystem_foobar2000() {}
 
-    // 核心协议处理
     bool is_our_path(const char* p_path) override;
     bool get_canonical_path(const char* p_path, pfc::string_base& p_out) override;
     bool get_display_path(const char* p_path, pfc::string_base& p_out) override;
 
-    // 文件操作 (来自 filesystem 基类)
     void open(service_ptr_t<file>& p_out, const char* p_path, t_open_mode p_mode, abort_callback& p_abort) override;
     void remove(const char* p_path, abort_callback& p_abort) override;
     void move(const char* p_src, const char* p_dst, abort_callback& p_abort) override;
     
-    // 状态与属性 (来自 filesystem_v2)
     bool is_remote(const char* p_src) override;
     
-    // filesystem_v3 要求的纯虚函数
     t_filestats2 get_stats2(const char* p_path, uint32_t s2flags, abort_callback& p_abort) override;
     void list_directory_v3(const char* path, directory_callback_v3& callback, unsigned listMode, abort_callback& p_abort) override;
     
-    // 目录操作 (来自 filesystem_v2)
     void make_directory(const char* path, abort_callback& abort, bool* didCreate = nullptr) override;
     
-    // 其他接口要求的方法
     bool supports_content_types() override;
     char pathSeparator() override;
 
