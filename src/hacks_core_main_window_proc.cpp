e021cf6f76ad83a0f688ea61d7fb6c0d8e913b31
YeYo <16457576+simear2004@users.noreply.github.com>
2026-04-14 11:14:35 +0800

    修复：窗口最大化时白色背景闪烁问题

--- src/hacks_core_main_window_proc.cpp
+++ src/hacks_core_main_window_proc.cpp
@@ -154,6 +154,9 @@ LRESULT OpenHacksCore::OpenHacksMainWindowProc(HWND wnd, UINT msg, WPARAM wp, LP
 {
     switch (msg)
     {
+    case WM_ERASEBKGND: // Fix: White background flickering issue when maximizing
+        return 1; // end
+        
     case WM_SYSCOMMAND:
         if (OnSysCommand(wnd, wp, lp))
             return 0;
