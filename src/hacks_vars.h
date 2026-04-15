1697277a4fee527d350e2b4541019ae1793bf589
YeYo <16457576+simear2004@users.noreply.github.com>
2026-04-15 18:46:53 +0800

    Update hacks_vars.h

--- src/hacks_vars.h
+++ src/hacks_vars.h
@@ -101,15 +101,13 @@ struct WindowStateData
     bool fullscreen = false;
     DWORD style = 0;
     WINDOWPLACEMENT wp = { sizeof(WINDOWPLACEMENT) };
-    bool wasCustomMaximized = false;
-    int32_t reserved[7] = {};
+    int32_t reserved[8] = {};
 
     void FromWindowState(const WindowState& state)
     {
         fullscreen = state.fullscreen;
         style = state.style;
         wp = state.wp;
-        wasCustomMaximized = state.wasCustomMaximized;
     }
 
     WindowState ToWindowState() const
@@ -118,7 +116,6 @@ struct WindowStateData
         state.fullscreen = fullscreen;
         state.style = style;
         state.wp = wp;
-        state.wasCustomMaximized = wasCustomMaximized;
         return state;
     }
 };
